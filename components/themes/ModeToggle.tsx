cc64c6915a67c01d00dedf0ba1938a19d6ee2416
Maithy-a <maithyabon2@gmail.com>
2025-07-09 01:18:39 +0300

    Refactor ModeToggle component for improved readability and structure

--- components/themes/ModeToggle.tsx
+++ components/themes/ModeToggle.tsx
@@ -1,24 +1,31 @@
-"use client"
-import React from 'react'
-import { IconBrightness } from '@tabler/icons-react';
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { IconBrightness } from '@tabler/icons-react'
 import { Button } from "@/components/ui/button"
 import { useTheme } from "next-themes"
 
 const ModeToggle = () => {
+    const { theme, setTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
 
-    const { setTheme } = useTheme()
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) return null
 
     return (
-        <div>
-            <Button
-                onClick={() => setTheme(theme => theme === "light" ? "dark" : "light")}
-                variant='secondary'
-                size="icon"
-                className='size-8'>
-                <IconBrightness />
-                <span className="sr-only">Toggle theme</span>
-            </Button>
-        </div>
+        <Button
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            variant="secondary"
+            size="icon"
+            className="size-8"
+        >
+            <IconBrightness />
+            <span className="sr-only">Toggle theme</span>
+        </Button>
     )
 }
 
