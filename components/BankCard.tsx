c503a6810ff31d5d5d903e0f30afff03397644da
Maithy-a <maithyabon2@gmail.com>
2025-07-08 15:48:05 +0300

    Implement full layout and UI for the bankcard

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -3,36 +3,33 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
+
 const BankCard = (
     { account, userName, showBalance = true }: CreditCardProps
 ) => {
     return (
         <div className='flex flex-col' >
             <Link href={'/'} className='bank-card' >
-                <div className="bank-card_content">
+
+                <div className="bank-card_content font-semibold text-white">
                     <div>
-                        <h1 className="text-[16px] font-semibold text-white">
-                            {account.name || userName}
+                        <h1 className="text-[17px] tracking-widest font-qurova mb-2 ">
+                            Chainfunds
                         </h1>
-                        <p className="text-[18px] font-semibold text-white mt-2">
+                        <p className="text-[16px]">
                             {formatAmount(account.currentBalance)}
                         </p>
                     </div>
-                    <article className="flex flex-col gap-2 mt-9">
+                    <article className="flex flex-col gap-2 mt-9 text-[13px]">
                         <div className="flex justify-between">
-                            <h1 className='text-[13px] font-semibold' >
+                            <h1 className='text-[13.5px]' >
                                 {userName}
                             </h1>
-                            <h2 className='text-[15px] font-semibold' >
-                                ** / **
+                            <h2>
+                                •• / ••
                             </h2>
                         </div>
-                        <p className='text-[15px] font-semibold tracking-[1.1px]'>
-                            **** **** ****
-                            <span className='text-[12px]' >
-                                {account.mask}
-                            </span>
-                        </p>
+                        <p className='tracking-[1.2px] '> •••• •••• •••• <span>  {/* {account.mask} */}1234 </span></p>
                     </article>
                 </div>
 
@@ -46,19 +43,22 @@ const BankCard = (
                     <Image
                         src={'/icons/mastercard.svg'}
                         width={40}
-                        height={25}
-                        alt='mastercard'                       
-                        />
-                </div>
-
-                <div className="absolute inset-0 z-0">
-                    <Image
-                        src="/icons/lines.svg"
-                        alt="line"
-                        fill className="object-cover"
+                        height={20}
+                        alt='mastercard'
                     />
                 </div>
+
+                {/* <div className="inset">
+                    <Image
+                        src="/gradient/Worldmap.svg"
+                        alt="line"
+                        fill className="object-cover opacity-20 "
+                    />
+                </div> */}
             </Link>
+
+            {/* COPY */}
+
         </div>
     )
 }
